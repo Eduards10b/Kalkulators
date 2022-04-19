06d2d869a1244840195a45529f417431a05ab3ec
Eduards10b <eduards.kasparovics@gmail.com>
2022-04-19 09:34:37 +0300

    Otrdiena

--- kalk.py
+++ kalk.py
@@ -1,14 +1,54 @@
+from cmath import e
 from tkinter import*
 mLogs=Tk()
 mLogs.title("Kalkulators")
 #mLogs.geometry("300x300")
 
+def btnCommand(command):
+    global num1
+    global mathOp
+    mathOp=command
+    num1=int(e.get())
+    e.delete(0,END)
+    return 0
+
+
 def btnClick(number):
     current=e.get()#nolasa esošo skaitli
     e.delete(0,END)#nodzēš
     newNumber=str(current)+str(number)
-    e.insert(0,newNumber)
+    e.insert(0,newNumber)#ievieto displeja
     return 0
+
+def vienads():
+    global num2
+    num2=(int(e.get()))
+    result=0
+    if mathOp=="+":
+        result=num1+num2
+    elif mathOp=="*":
+        result=num1*num2
+    elif mathOp=="/":
+        result=num1/num2
+    elif mathOp=="-":
+        result=num1-num2
+    else:
+        result=0
+    e.delete(0,END)            
+    e.insert(0,str(result))
+    return 0
+
+def notirit():
+    e.delete(0,END)
+    num=1
+    mathOp=''
+    return 0
+
+
+
+
+
+
 
 e=Entry(mLogs,width=15,font=("Arial black",20))
 e.grid(row=0,column=0,columnspan=4)
@@ -45,12 +85,12 @@ btn2.grid(row=3,column=1)
 btn3.grid(row=3,column=2)
 btn0.grid(row=4,column=0)
 
-btn10=Button(mLogs, text="/",padx="40",pady="20",command=lambda:btnClick("/"))
-btn11=Button(mLogs, text="x",padx="40",pady="20",command=lambda:btnClick("x"))
-btn12=Button(mLogs, text="+",padx="40",pady="20",command=lambda:btnClick("+"))
-btn13=Button(mLogs, text="-",padx="40",pady="20",command=lambda:btnClick("-"))
-btn14=Button(mLogs, text="=",padx="40",pady="20",command=lambda:btnClick("="))
-btn15=Button(mLogs, text="C",padx="40",pady="20",command=lambda:btnClick("C"))
+btn10=Button(mLogs, text="/",padx="40",pady="20",command=lambda:btnCommand("/"))
+btn11=Button(mLogs, text="x",padx="40",pady="20",command=lambda:btnCommand("*"))
+btn12=Button(mLogs, text="+",padx="40",pady="20",command=lambda:btnCommand("+"))
+btn13=Button(mLogs, text="-",padx="40",pady="20",command=lambda:btnCommand("-"))
+btn14=Button(mLogs, text="=",padx="40",pady="20",command=vienads)
+btn15=Button(mLogs, text="C",padx="40",pady="20",command=notirit)
 
 btn10.grid(row=1,column=3)
 btn11.grid(row=2,column=3)
@@ -59,13 +99,6 @@ btn13.grid(row=4,column=3)
 btn14.grid(row=4,column=1)
 btn15.grid(row=4,column=2)
 
-def btnCommand(command):
-    global num1
-    global matchOp
-    mathOp=command
-    num1=int(e.get())
-    e.delete(0,END)
-    return 0
 
 
 
